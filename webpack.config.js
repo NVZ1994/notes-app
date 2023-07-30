d19372dd67ede6a60a623bf4daacc72e7c255404
NVZ1994 <nikita.zhuk.1994@gmail.com>
2023-07-30 21:45:08 +0300

    added mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+  mode: "production",
   entry: "./index.js",
   output: {
     filename: "bundle.js",
